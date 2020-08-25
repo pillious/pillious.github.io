a304c1d33c8250464aa39e803704cf6074895f14
Andrew <andrewzhlee@gmail.com>
2020-08-25 00:36:58 -0400

    complete dark mode styles

--- assets/scripts/dark.js
+++ assets/scripts/dark.js
@@ -2,41 +2,24 @@ var localStorage = window.localStorage;
 var colorMode;
 
 $(document).ready(function () {
-    colorMode = getInitialColorMode();
-    console.log(colorMode);
+    colorMode = document.documentElement.dataset.theme;
 });
 
-function getInitialColorMode() {
-    const colorPreference = localStorage.getItem('mode');
-    const hasColorPreference = typeof colorPreference === 'string';
-
-    // Check if the user has explicitly chosen light or dark. 
-    // If not, this value will be null.
-    if (hasColorPreference) {
-        return colorPreference;
+function toggleColorMode() {
+    if (colorMode === 'light') {
+        document.documentElement.setAttribute("data-theme", "dark");
+        updateColorPreference('dark');
+    }
+    else {
+        document.documentElement.setAttribute("data-theme", "light");
+        updateColorPreference('light');
     }
 
-    // If they haven't been explicit, let's check the media query
-    // mql -> media query list
-    const mql = window.matchMedia('(prefers-color-scheme: dark)');
-    const hasMediaQueryPreference = typeof mql.matches === 'boolean';
-    if (hasMediaQueryPreference) {
-        return mql.matches ? 'dark' : 'light';
-    }
-
-    // If browser/OS that doesn't support color themes, return 'light' as default.
-    return 'light';
 }
 
-function updateColorPreference() {
-    // toggle sections between light and dark.
-    $('.about-wrapper').toggleClass('dark');
-    $('#projects').toggleClass('dark');
-
-    colorMode = colorMode === 'light' ? colorMode = 'dark' : colorMode = 'light';
+function updateColorPreference(mode) {
+    colorMode = mode;
     saveColorPreference(colorMode);
-
-    console.log(colorMode);
 }
 
 function saveColorPreference(mode) {
